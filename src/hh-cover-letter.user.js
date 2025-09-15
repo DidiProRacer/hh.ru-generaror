1b3643c387a30953ce416134b5f17ed5c9f2a9fa
DidiProRacer <139759036+DidiProRacer@users.noreply.github.com>
2025-09-15 16:23:13 +0300

    Create hh-cover-letter.user.js

---
+++ src/hh-cover-letter.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Custom Cover Letter Generator
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Generate custom cover letters automatically
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+})();
